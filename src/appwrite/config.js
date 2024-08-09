999d582036ab4b46a064521f3bcc2fdea411418a
vivek thorat <vivekthorat588@gmail.com>
2024-08-09 22:27:44 +0530

    fix: Env variables

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,16 +1,16 @@
 import { Client, Databases } from "appwrite";
  
 const client = new Client()
-    .setEndpoint(process.env.VITE_ENDPOINT)
-    .setProject(process.env.VITE_PROJECT_ID);
+    .setEndpoint(import.meta.env.VITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_PROJECT_ID);
  
 const databases = new Databases(client);
 
 const collections = [
   {
       name: "notes",
-      id: process.env.VITE_COLLECTION_NOTES_ID,
-      dbId: process.env.VITE_DATABASE_ID
+      id: import.meta.env.VITE_COLLECTION_NOTES_ID,
+      dbId: import.meta.env.VITE_DATABASE_ID
   },
 ];
  
